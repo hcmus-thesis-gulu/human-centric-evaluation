c3bd2c4e507fa86bd40d4a978ce01d9d864b2b7a
Hai-Dang Huynh-Lam <danghailamhuynh@gmail.com>
2023-06-12 18:15:56 +0700

    Prepare for running f-score eval

--- classic/evaluation.py
+++ classic/evaluation.py
@@ -7,61 +7,106 @@ from scipy.io import loadmat
 from evaluator import evaluateSummary
 
 
-# TODO: Storing the video-level result to a json in result folder
-def evaluateSummaries(groundtruth_folder, summary_folder,
-                      result_folder, mode):
-    f_scores = {'scores': {}}
+def evaluateSummaries(groundtruth_folder, summary_folder, result_folder, mode):
+    f_measures = {}
+    
     for groundtruth_name in os.listdir(groundtruth_folder):
         if groundtruth_name.endswith('.mat'):
-            grountruth_file = os.path.splitext(groundtruth_name)[0]
+            filename = os.path.splitext(groundtruth_name)[0]
             groundtruth_path = os.path.join(groundtruth_folder,
                                             groundtruth_name)
             groundtruth = loadmat(groundtruth_path)
             
             user_summary = groundtruth['user_summary']
-            segments = os.path.join(summary_folder,
-                                    grountruth_file + '_segments.npy')
+            scores_path = os.path.join(summary_folder,
+                                       filename + '_scores.npy')
+            keyframes_path = os.path.join(summary_folder,
+                                          filename + '_keyframes.npy')
             
-            if os.path.exists(segments):
-                machine_parts = np.load(segments)
-                f_score, f_measure = evaluateSummary(machine_parts,
-                                                     user_summary,
-                                                     mode)
+            if os.path.exists(scores_path):
+                scores = np.load(scores_path)
+                keyframe_indices = np.load(keyframes_path)
                 
-                f_scores['scores'][grountruth_file] = f_score
-                print(f'F-measure of {grountruth_file} is {f_measure}')
+                # file_end = '_reduced.npy' if reduced_emb else '_embeddings.npy'
+                # embedding_file = filename + file_end
+                # embedding_path = os.path.join(embedding_folder, embedding_file)
+                # embeddings = np.load(embedding_path)
                 
-                result_file = os.path.join(result_folder,
-                                           grountruth_file + '_feval.npy')
-                np.save(result_file, f_measure)
+                # eval_results = evaluateSummary(machine_embeddings=embeddings,
+                #                                machine_parts=scores,
+                #                                user_summary=user_summary,
+                #                                method=method,
+                #                                mode=mode
+                #                                )
+                
+                eval_results = evaluateSummary(scores=scores,
+                                               user_summary=user_summary,
+                                               keyframe_indices=keyframe_indices,
+                                               mode=mode
+                                               )
+                
+                f_score, f_scores, summary_lengths, summary_length = eval_results
+                
+                f_measures[filename] = {
+                    'f_score': f_score,
+                    'f_scores': f_scores,
+                    'summary_lengths': summary_lengths,
+                    'summary_length': summary_length,
+                    'video_length': len(user_summary),
+                    'summarized_rate': summary_length / len(user_summary)
+                }
+                
+                print(f'F-measure of {filename} is {f_score}')
     
-    if f_scores:
-        avg_f_score = sum(f_scores) / len(f_scores)
-        f_scores['average'] = avg_f_score
-        print(f'Average F-measure: {avg_f_score:.4f}')
+    if f_measures:
+        f_measure = np.mean([f_measures[key]['f_score']
+                             for key in f_measures])
         
-    json_file = os.path.join(result_folder, 'f_scores.json')
+        mean_sum_rate = np.mean([f_measures[key]['summarized_rate']
+                                 for key in f_measures])
+        var_sum_rate = np.var([f_measures[key]['summarized_rate']
+                               for key in f_measures])
+        
+        results = {
+            'f_measures': f_measures,
+            'average_f_measure': f_measure,
+            'dist_summarized_rate': {
+                'mean': mean_sum_rate,
+                'var': var_sum_rate,
+            }
+        }
+        
+        print(f'Average F-measure: {f_measure:.4f}')
+        
+    json_file = os.path.join(result_folder, 'results.json')
     with open(json_file, 'w', encoding='utf-8') as file:
-        json.dump(f_scores, file)
+        json.dump(results, file)
 
 
 def evaluate():
     parser = argparse.ArgumentParser(description='Evaluate machine learning algorithm summaries.')
     parser.add_argument('--groundtruth-folder', type=str, required=True,
                         help='Path to the folder containing groundtruth .mat files')
+    # parser.add_argument('--embedding-folder', type=str, required=True,
+    #                     help='Path to the folder containing output .npy files')
     parser.add_argument('--summary-folder', type=str, required=True,
                         help='Path to the folder containing output .npy files')
     parser.add_argument('--result-folder', type=str, default='result',
                         help='Path to the folder containing the result of evaluation')
     
+    # parser.add_argument('--reduced-emb', action='store_true',
+    #                     help='Whether to use reduced embeddings')
     parser.add_argument('--mode', type=str, default='frame',
                         help='Evaluation mode: "frame" or "fragment"')
+    # parser.add_argument('--method', type=str, default='middle',
+    #                     help='Method to evaluate: "middle" or "mean"')
     
     args = parser.parse_args()
     evaluateSummaries(groundtruth_folder=args.groundtruth_folder,
                       summary_folder=args.summary_folder,
                       result_folder=args.result_folder,
-                      mode=args.mode)
+                      mode=args.mode
+                      )
 
 
 if __name__ == '__main__':
--- classic/evaluator.py
+++ classic/evaluator.py
@@ -1,51 +1,83 @@
 import numpy as np
+from utils import mean_embeddings, similarity_score
 
 
-# For each segment, compute the mean features and
-# similarity of all features with the mean
-def extractSummary(self, embeddings, segments):
-    segment_scores = []
+# # For each segment, compute the mean features and
+# # similarity of all features with the mean
+# def extractSummary(embeddings, segments, method):
+#     segment_scores = []
+#     keyframe_indices = []
     
-    for _, start, end in segments:
-        # Get the associated features
-        segment_features = embeddings[start:end]
+#     for _, start, end in segments:
+#         # Get the associated features
+#         segment_features = embeddings[start:end]
         
-        # Calculate the similarity with representative
-        if self.representative == "mean":
-            mean = mean_embeddings(segment_features)
-        elif self.representative == "middle":
-            mean = segment_features[len(segment_features) // 2]
+#         # Calculate the similarity with representative
+#         if method == "mean":
+#             mean = mean_embeddings(segment_features)
+#         elif method == "middle":
+#             mean = segment_features[len(segment_features) // 2]
         
-        score = similarity_score(segment_features, mean)
-        segment_scores.extend(score.tolist())
+#         score = similarity_score(segment_features, mean)
+#         segment_scores.extend(score.tolist())
+        
+#         # Select the representative with the highest similarity
+#         keyframe_index = np.argmax(score) + start
+#         keyframe_indices.append(keyframe_index)
     
-    return np.asarray(segment_scores)
+#     return np.asarray(segment_scores), np.asarray(keyframe_indices).sort()
 
 
-def computeSummary(segments, length, method='max'):
-    if length < len(segments):
-        selection_step = len(segments) / length
-        selected_parts = segments[::selection_step]
+# def computeSummary(embeddings, segments, length, method):
+#     scores, key_indices = extractSummary(embeddings, segments, method)
+    
+#     if length < len(key_indices):
+#         selection_step = len(key_indices) / length
+#         selections = key_indices[::selection_step]
+#         return selections
+#     else:
+#         unselected_scores = np.delete(scores, key_indices)
+#         selection = np.argpartition(unselected_scores,
+#                                     -length)[-length:]
         
-        # Select representative of the selected parts
-        selection = extractSummary(selected_parts, method)
-        return selection
+#         # Final selections are the keyframes and the selected frames
+#         selections = np.concatenate((key_indices, selection))
+#         selections.sort()
+        
+#         return selections
+
+
+def computeSummary(scores, keyframe_indices, length):
+    if length < len(keyframe_indices):
+        selection_step = (len(keyframe_indices) // length) + 1
     else:
-        # Select representative of all parts
-        summary = extractSummary(segments, method)
-        unselected
-        selection =  np.argpartition(scores, -length)[-length:]
-        return selection
+        selection_step = 1
+        
+    selection = keyframe_indices[::selection_step]
+    remained_length = length - len(selection)
+    assert remained_length >= 0
+    
+    unselected_scores = np.delete(scores, selection)
+    other_selection = np.argpartition(unselected_scores,
+                                      -remained_length)[-remained_length:]
+    
+    selections = np.concatenate((selection, other_selection))
+    return np.sort(selections)
 
 
-def evaluateSummary(machine_parts, user_summary, mode='frame'):
-    f_measures = []
+def evaluateSummary(scores, user_summary, keyframe_indices, mode='frame'):
+    f_scores = []
+    lengths = []
+    
     for user in range(user_summary.shape[1]):
         user_selected = np.where(user_summary[:, user] > 0)[0]
         
         if mode == 'frame':
             length = len(user_selected)
-            machine_selected = computeSummary(machine_parts, length)
+            machine_selected = computeSummary(scores=scores,
+                                              keyframe_indices=keyframe_indices,
+                                              length=length,
+                                              )
             
             tp = len(np.intersect1d(machine_selected, user_selected))
             fp = len(np.setdiff1d(machine_selected, user_selected))
@@ -53,7 +85,10 @@ def evaluateSummary(machine_parts, user_summary, mode='frame'):
         elif mode == 'fragment':
             user_fragments = np.unique(user_summary[:, user])
             length = len(user_fragments)
-            machine_selected = computeSummary(machine_parts, length)
+            machine_selected = computeSummary(scores=scores,
+                                              keyframe_indices=keyframe_indices,
+                                              length=length,
+                                              )
             
             intersected_indices = np.intersect1d(machine_selected,
                                                  user_selected)
@@ -64,11 +99,13 @@ def evaluateSummary(machine_parts, user_summary, mode='frame'):
         
         precision = tp / (tp + fp) if tp + fp > 0 else 0
         recall = tp / (tp + fn) if tp + fn > 0 else 0
-        f_measure = 2 * precision * recall / (precision + recall) if precision + recall > 0 else 0
+        f_score = 2 * precision * recall / (precision + recall) if precision + recall > 0 else 0
         
-        f_measures.append(f_measure)
+        f_scores.append(f_score)
+        lengths.append(length)
     
     # Maximum F-measure across all users
-    f_score = max(f_measures)
+    f_score = max(f_scores)
+    summary_length = lengths[np.argmax(f_scores)]
     
-    return f_score, f_measures
+    return f_score, f_scores, lengths, summary_length
