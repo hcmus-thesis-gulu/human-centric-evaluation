f8306d61f91417542f634f067014ebd005b98359
Hai-Dang Huynh-Lam <danghailamhuynh@gmail.com>
2023-06-16 20:47:47 +0700

    Tested materialization

--- preprocess/machine_video.py
+++ preprocess/machine_video.py
@@ -5,41 +5,25 @@ import numpy as np
 from preprocess.utils import broadcast_video
 
 
-def materialize_video(video_folder, embedding_folder, context_folder,
-                      demo_folder, video_name, max_length, sum_rate,
-                      fps=None):
-    sample_file = os.path.join(embedding_folder, f'{video_name}_samples.npy')
-    keyframe_file = os.path.join(context_folder, f'{video_name}_keyframes.npy')
+def materialize_video(video_folder, summary_folder, demo_folder, video_name,
+                      max_length, sum_rate, fps=None):
+    keyframe_file = os.path.join(summary_folder, f'{video_name}_keyframes.npy')
+    keyframes = np.load(keyframe_file)
     
-    sample_video_path = os.path.join(demo_folder, f'{video_name}_sample.avi')
     keyframe_video_path = os.path.join(demo_folder, f'{video_name}_keyframes.avi')
     raw_video_path = os.path.join(video_folder, f'{video_name}.mp4')
     
-    try:
-        samples = np.load(sample_file)
-        broadcast_video(input_video_path=raw_video_path,
-                        frame_indices=samples,
-                        output_video_path=sample_video_path,
-                        max_length=max_length,
-                        sum_rate=sum_rate,
-                        fps=fps
-                        )
-        
-        keyframes = np.load(keyframe_file)
-        broadcast_video(input_video_path=raw_video_path,
-                        frame_indices=keyframes,
-                        output_video_path=keyframe_video_path,
-                        max_length=max_length,
-                        sum_rate=sum_rate,
-                        fps=fps
-                        )
-    except Exception as error:
-        print(error)
-        print(f'{video_name} not found')
+    broadcast_video(input_video_path=raw_video_path,
+                    frame_indices=keyframes,
+                    output_video_path=keyframe_video_path,
+                    max_length=max_length,
+                    sum_rate=sum_rate,
+                    fps=fps
+                    )
         
 
-def materialize_videos(video_folder, embedding_folder, context_folder,
-                       demo_folder, max_length, sum_rate, fps=None):
+def materialize_videos(video_folder, summary_folder, demo_folder, max_length,
+                       sum_rate, fps=None):
     video_files = os.listdir(video_folder)
     
     for video_file in video_files:
@@ -47,8 +31,7 @@ def materialize_videos(video_folder, embedding_folder, context_folder,
         print(f'Processing {video_name}')
         
         materialize_video(video_folder=video_folder,
-                          embedding_folder=embedding_folder,
-                          context_folder=context_folder,
+                          summary_folder=summary_folder,
                           demo_folder=demo_folder,
                           video_name=video_name,
                           max_length=max_length,
@@ -60,9 +43,7 @@ def main():
     parser = argparse.ArgumentParser(description='Visualize result')
     parser.add_argument('--video-folder', type=str, required=True,
                         help='Path to folder containing videos')
-    parser.add_argument('--embedding-folder', type=str, required=True,
-                        help='path to folder containing feature files')
-    parser.add_argument('--context-folder', type=str, required=True,
+    parser.add_argument('--summary-folder', type=str, required=True,
                         help='path to output folder for clustering')
     parser.add_argument('--demo-folder', type=str, required=True,
                         help='path to folder saving demo videos')
@@ -76,8 +57,7 @@ def main():
     args = parser.parse_args()
     
     materialize_videos(video_folder=args.video_folder,
-                       embedding_folder=args.embedding_folder,
-                       context_folder=args.context_folder,
+                       summary_folder=args.context_folder,
                        demo_folder=args.demo_folder,
                        max_length=args.max_length,
                        sum_rate=args.sum_rate,
