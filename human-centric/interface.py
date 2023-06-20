77bb8b2034f8ed4840c2bda60775a2d82fb51a6b
Phuong Ho <annawu0611@gmail.com>
2023-06-20 12:52:09 +0700

    pywebio

--- human-centric/interface.py
+++ human-centric/interface.py
@@ -1,50 +1,97 @@
-import streamlit as st
-import json
-import random
+import os
+import cv2
 
-# Set the title of the app
-st.title("Video and Multiple Choice Questions")
+src = 'videos/original'
+dst = 'videos/original'
 
-# Load the videos and questions from a JSON file
-with open("videos_and_questions.json", "r") as f:
-    videos_and_questions = json.load(f)
+def convert_to_mp4(input_file, output_file):
+    video = cv2.VideoCapture(input_file)
+    if (video.isOpened() == False): 
+        print("Error reading video file")
 
-# Ask the user how many videos they want to watch
-num_videos = st.number_input("How many videos do you want to watch?", min_value=1, max_value=len(videos_and_questions))
+    frame_width = int(video.get(3))
+    frame_height = int(video.get(4))
+   
+    size = (frame_width, frame_height)
+    fps = video.get(cv2.CAP_PROP_FPS)
+   
+    result = cv2.VideoWriter(output_file, 
+                         cv2.VideoWriter_fourcc(*'MP4V'),
+                         fps, size)
+    
+    while(True):
+        ret, frame = video.read()
+    
+        if ret == True: 
+            result.write(frame)
+    
+            # cv2.imshow('Frame', frame)
+            # if cv2.waitKey(1) & 0xFF == ord('s'):
+            #     break
+        else:
+            break
+  
+    video.release()
+    result.release()
+    
+    cv2.destroyAllWindows()
+   
+    print("The video was successfully saved")
 
-# Randomly sample the specified number of videos without replacement
-selected_videos_and_questions = random.sample(videos_and_questions, num_videos)
+for filename in os.listdir(src):
+    filename = filename.split(".")[0] + ".{ext}"
+    input = os.path.join(src, filename.format(ext='webm'))
+    output = os.path.join(dst, filename.format(ext='mp4'))
+    convert_to_mp4(input, output)
+    print(output)
 
-# Initialize an empty list to store all of the user's answers
-all_user_answers = []
+# import streamlit as st
+# import json
+# import random
 
-# Loop through the selected videos and their corresponding questions
-for video_and_questions in selected_videos_and_questions:
-    # Display the video
-    st.video(video_and_questions["video"])
+# # Set the title of the app
+# st.title("Video and Multiple Choice Questions")
 
-    # Initialize an empty list to store the user's answers for this video
-    user_answers = []
+# # Load the videos and questions from a JSON file
+# with open("videos_and_questions.json", "r") as f:
+#     videos_and_questions = json.load(f)
 
-    # Loop through the questions and display them with their options
-    for question in video_and_questions["questions"]:
-        st.write(question["question"])
-        answer = st.radio("", question["options"])
-        user_answers.append(answer)
+# # Ask the user how many videos they want to watch
+# num_videos = st.number_input("How many videos do you want to watch?", min_value=1, max_value=len(videos_and_questions))
 
-    # Add the user's answers for this video to the list of all answers
-    all_user_answers.append({
-        "video": video_and_questions["video"],
-        "answers": user_answers
-    })
+# # Randomly sample the specified number of videos without replacement
+# selected_videos_and_questions = random.sample(videos_and_questions, num_videos)
 
-    # Display the user's answers for this video
-    st.write("Your answers for this video:")
-    st.write(user_answers)
+# # Initialize an empty list to store all of the user's answers
+# all_user_answers = []
 
-# Save all of the user's answers to a JSON file
-with open("user_answers.json", "w") as f:
-    json.dump(all_user_answers, f)
+# # Loop through the selected videos and their corresponding questions
+# for video_and_questions in selected_videos_and_questions:
+#     # Display the video
+#     st.video(video_and_questions["video"])
 
-# Add a message at the end
-st.write("Thank you for participating! Your answers have been saved.")
+#     # Initialize an empty list to store the user's answers for this video
+#     user_answers = []
+
+#     # Loop through the questions and display them with their options
+#     for question in video_and_questions["questions"]:
+#         st.write(question["question"])
+#         answer = st.radio("", question["options"])
+#         user_answers.append(answer)
+
+#     # Add the user's answers for this video to the list of all answers
+#     all_user_answers.append({
+#         "video": video_and_questions["video"],
+#         "answers": user_answers
+#     })
+
+#     # Display the user's answers for this video
+#     st.write("Your answers for this video:")
+#     st.write(user_answers)
+
+# # Save all of the user's answers to a JSON file
+# with open("user_answers.json", "w") as f:
+#     json.dump(all_user_answers, f)
+
+# # Add a message at the end
+# st.write("Thank you for participating! Your answers have been saved.")
--- human-centric/main.py
+++ human-centric/main.py
@@ -1,4 +1,51 @@
-import streamlit as st
+# import os
+# import cv2
+
+# src = 'videos/user-summaries'
+# dst = 'videos/user-summaries'
+
+# def convert_to_mp4(input_file, output_file):
+#     video = cv2.VideoCapture(input_file)
+#     if (video.isOpened() == False): 
+#         print("Error reading video file")
+
+#     frame_width = int(video.get(3))
+#     frame_height = int(video.get(4))
+   
+#     size = (frame_width, frame_height)
+#     fps = video.get(cv2.CAP_PROP_FPS)
+   
+#     result = cv2.VideoWriter(output_file, 
+#                          cv2.VideoWriter_fourcc(*'MP4V'),
+#                          fps, size)
+    
+#     while(True):
+#         ret, frame = video.read()
+    
+#         if ret == True: 
+#             result.write(frame)
+    
+#         else:
+#             break
+  
+#     video.release()
+#     result.release()
+    
+#     cv2.destroyAllWindows()
+   
+#     print("The video was successfully saved")
+
+# for filename in os.listdir(src):
+#     filename = filename.split(".")[0] + ".{ext}"
+#     input = os.path.join(src, filename.format(ext='avi'))
+#     output = os.path.join(dst, filename.format(ext='mp4'))
+#     convert_to_mp4(input, output)
+#     print(output)
+
+import json
+from pywebio import input
+from pywebio import output
+import pywebio_battery
 import json
 import random
 from uuid import uuid4
@@ -7,44 +54,29 @@ common_json = "human-centric/common-questions.json"
 comparison_json = "human-centric/comparison-questions.json"
 questions_json = "human-centric/questions.json"
 survey_info_json = "human-centric/survey-information.json"
-original_path = "videos/original/{name}.webm"
-user_path = "videos/user-summaries/{name}_{user_idx}.avi"
-our_path = "videos/user-summaries/{name}_1.avi"
+original_path = "videos/original/{name}.mp4"
+user_path = "videos/user-summaries/{name}_{user_idx}.mp4"
+our_path = "videos/user-summaries/{name}_1.mp4"
 answers_path = "human-centric/answers/{uuid}.json"
 
-universal_index = 0
-
 def short_answer_question(question):
-    global universal_index
-    answer = st.text_input(question, key=universal_index)
-    universal_index += 1
+    answer = input.input(question)
     return answer
 
 def paragraph_question(question):
-    global universal_index
-    answer = st.text_area(question, key=universal_index)
-    universal_index += 1
+    answer = input.textarea(question)
     return answer
 
 def multiple_choice_question(question, options):
-    global universal_index
-    answer = st.radio(question, options, key=universal_index)
-    universal_index += 1
+    answer = input.radio(question, options)
     return answer
 
 def checkbox_question(question, options):
-    global universal_index
-    st.write(question)
-    answers = []
-    for option in options:
-        answers.append(st.checkbox(option, key=universal_index))
-        universal_index += 1
+    answers = input.checkbox(question, options)
     return answers
 
 def linear_scale_question(question, lower_bound=0, upper_bound=10):
-    global universal_index
-    answer = st.slider(question, lower_bound, upper_bound, key=universal_index)
-    universal_index += 1
+    answer = input.slider(question, value=lower_bound, min_value=lower_bound, max_value=upper_bound)
     return answer
 
 def ask_question(question):
@@ -58,28 +90,21 @@ def ask_question(question):
             return checkbox_question(question['question'], question['options'])
         case 'linear':
             return linear_scale_question(question['question'])
-
-def ask_question(question):
-    match question["type"]:
-        case "short":
-            return short_answer_question(question["question"])
-        case "multiple":
-            return multiple_choice_question(question["question"], question["options"])
-        case "checkbox":
-            return checkbox_question(question["question"], question["options"])
-        case "linear":
-            return linear_scale_question(question["question"])
-
+        
 def broadcast_video(video_path):
     with open(video_path, "rb") as f:
         video_bytes = f.read()
-    st.video(video_bytes)
+    pywebio_battery.put_video(video_path)
+    print(video_path)
+    
+    # url = "https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
+    # pywebio_battery.put_video(url)
 
 def present_comparison(video_infor, comparison_questions):
-    st.subheader("Original Video")
+    output.put_markdown("### Original Video")
     print(video_infor)
     broadcast_video(original_path.format(name=video_infor['name']))
-    st.subheader("Summary Video")    
+    output.put_markdown("### Summary Video")    
     user_idx = random.randint(0, 15)
     if user_idx:
         broadcast_video(user_path.format(name=video_infor['name'], user_idx=user_idx))
@@ -109,12 +134,11 @@ def present_normal(video_infor, common_questions):
         
     return answers
 
-
-def main():
-# original_path = "videos/original/{name}.webm"
-# user_path = "videos/user-summaries/{name}_{user_idx}.avi"
-# our_path = "videos/user-summaries/{name}_1.avi"
-# answers_folder = "human-centric/answers"
+def survey():
+# # original_path = "videos/original/{name}.webm"
+# # user_path = "videos/user-summaries/{name}_{user_idx}.avi"
+# # our_path = "videos/user-summaries/{name}_1.avi"
+# # answers_folder = "human-centric/answers"
 
     with open(common_json, "r") as f:
         common_questions = json.load(f)
@@ -126,16 +150,15 @@ def main():
         survey_infor = json.load(f)
     survey_uuid = uuid4().hex
     survey_answers = []
-        
-    st.title(survey_infor["title"])
-    st.write(survey_infor["description"])
-    st.divider()
+    
+    output.put_markdown("# {title}".format(title=survey_infor["title"]))
+    output.put_text(survey_infor["description"])
     number_of_videos = linear_scale_question(survey_infor["number_of_videos"], 0, 25)
     
-    if st.button("Start the Survey"):
+    if number_of_videos:
         videos_to_ask = random.sample(video_infos, number_of_videos)
         for idx, video_infor in enumerate(videos_to_ask, start=1):
-            st.header("Video #{idx}".format(idx=idx))
+            output.put_markdown("## Video #{idx}".format(idx=idx))
             compare_or_normal = 0
             # random.randint(0, 1)
             if compare_or_normal:
@@ -143,11 +166,35 @@ def main():
             else:
                 answers = present_normal(video_infor, common_questions)
             survey_answers.append(answers)
+        
+if __name__ == '__main__':
+    survey()
 
-    if st.button("Submit Answers"):
-        print(survey_answers)
-        with open(answers_path.format(survey_uuid), "w") as f:
-            json.dump(survey_answers, f)
+
+
+
+# def main():
+
+
+
+        
     
-if __name__ == "__main__":
-    main()
+#     if input.button("Start the Survey"):
+#         videos_to_ask = random.sample(video_infos, number_of_videos)
+#         for idx, video_infor in enumerate(videos_to_ask, start=1):
+#             input.header("Video #{idx}".format(idx=idx))
+#             compare_or_normal = 0
+#             # random.randint(0, 1)
+#             if compare_or_normal:
+#                 answers = present_comparison(video_infor, comparison_questions)
+#             else:
+#                 answers = present_normal(video_infor, common_questions)
+#             survey_answers.append(answers)
+
+#     if input.button("Submit Answers"):
+#         print(survey_answers)
+#         with open(answers_path.format(survey_uuid), "w") as f:
+#             json.dump(survey_answers, f)
+    
+# if __name__ == "__main__":
+#     main()
