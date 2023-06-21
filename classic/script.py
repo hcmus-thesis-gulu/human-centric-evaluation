db99b8d961f4b8a2582102e850ac1038a0d7184e
Phuong Ho <annawu0611@gmail.com>
2023-06-21 14:57:01 +0700

    aaaaa

--- classic/script.py
+++ classic/script.py
@@ -1,27 +1,26 @@
 import os
 
 distance = "euclidean"
-embedding_dim = 2
+# embedding_dim = 3
 
 groundtruth_folder = "classic/data"
-summary_folder = "ablation/dino/summaries/{distance}/pca-tsne/embedding-{embedding_dim}/intermediate-{intermediate_dim}"
-result_folder = "ablation/dino/results/{distance}/pca-tsne/embedding-{embedding_dim}/intermediate-{intermediate_dim}"
+summary_folder = "ablation/dino-brute-force/summaries/{distance}/pca-only/embedding-{embedding_dim}"
+# /intermediate-{intermediate_dim}"
+result_folder = "ablation/dino-brute-force/{distance}/pca-only/embedding-{embedding_dim}"
+# /intermediate-{intermediate_dim}"
 
-for i in range(2, 8):
-    intermediate_dim = 1 << i
-    tmp_summary_folder = summary_folder.format(distance=distance, embedding_dim=embedding_dim, intermediate_dim=intermediate_dim)
-    tmp_result_folder = result_folder.format(distance=distance, embedding_dim=embedding_dim, intermediate_dim=intermediate_dim)
+for i in range(4, 129):
+    # intermediate_dim = 1 << i
+    embedding_dim = 1 << i
+    tmp_summary_folder = summary_folder.format(distance=distance, embedding_dim=embedding_dim)
+                                            #    , intermediate_dim=intermediate_dim)
+    tmp_result_folder = result_folder.format(distance=distance, embedding_dim=embedding_dim)
+                                            #  , intermediate_dim=intermediate_dim)
     os.mkdir(tmp_result_folder)
     
-    os.system("python classic/evaluation.py --groundtruth-folder {groundtruth_folder} --summary-folder {summary_folder} --result-folder {result_folder} --mode shot --coef 0 --expand 0.20 --fill-mode linear".format(
+    os.system("python classic/hypersearch.py --groundtruth-folder {groundtruth_folder} --summary-folder {summary_folder} --result-folder {result_folder} --mode shot --min-coef 0.0 --max-coef 0.1 --iter-coef 0.5  --min-expand 0.15 --max-expand 0.30 --iter-expand 0.05".format(
         groundtruth_folder=groundtruth_folder,
         summary_folder=tmp_summary_folder,
         result_folder=tmp_result_folder,
         
     ))
-    
-#     os.system("python scripts/summarization.py --embedding-folder {embedding_folder} --context-folder {context_folder} --summary-folder {summary_folder} --scoring-mode uniform --kf-mode middle-ends --reduced-emb --bias -1 --max-len 0".format(
-#         embedding_folder=embedding_folder,
-#         context_folder=tmp_context_folder,
-#         summary_folder=tmp_summary_folder
-#     ))
