a4fb181761a3523774d13ac4de4114696e7af536
Phuong Ho <annawu0611@gmail.com>
2023-06-21 10:28:03 +0700

    ablation final

--- classic/script.py
+++ classic/script.py
@@ -1,16 +1,16 @@
 import os
 
 distance = "euclidean"
-# embedding_dim = 2
+embedding_dim = 2
 
 groundtruth_folder = "classic/data"
-summary_folder = "ablation/dino/summaries/{distance}/pca-only/embedding-{embedding_dim}"
-result_folder = "ablation/dino/results/{distance}/pca-only/embedding-{embedding_dim}"
+summary_folder = "ablation/dino/summaries/{distance}/pca-tsne/embedding-{embedding_dim}/intermediate-{intermediate_dim}"
+result_folder = "ablation/dino/results/{distance}/pca-tsne/embedding-{embedding_dim}/intermediate-{intermediate_dim}"
 
-for i in range(2, 9):
-    embedding_dim = 1 << i
-    tmp_summary_folder = summary_folder.format(distance=distance, embedding_dim=embedding_dim)
-    tmp_result_folder = result_folder.format(distance=distance, embedding_dim=embedding_dim)
+for i in range(2, 8):
+    intermediate_dim = 1 << i
+    tmp_summary_folder = summary_folder.format(distance=distance, embedding_dim=embedding_dim, intermediate_dim=intermediate_dim)
+    tmp_result_folder = result_folder.format(distance=distance, embedding_dim=embedding_dim, intermediate_dim=intermediate_dim)
     os.mkdir(tmp_result_folder)
     
     os.system("python classic/evaluation.py --groundtruth-folder {groundtruth_folder} --summary-folder {summary_folder} --result-folder {result_folder} --mode shot --coef 0 --expand 0.20 --fill-mode linear".format(
